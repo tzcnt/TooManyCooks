911f3920286165c4fbe78eaafd270a2b69086c1d
tzcnt <trailingzerocount@gmail.com>
2024-04-09 21:23:39 -0700

    update wait_for_all_threads_to_sleep

--- include/tmc/detail/test.hpp
+++ include/tmc/detail/test.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "tmc/detail/thread_locals.hpp"
+#include "tmc/ex_cpu.hpp"
+
 #if defined(__x86_64__) || defined(_M_AMD64)
 #include <immintrin.h>
 #else
 #include <arm_acle.h>
 #endif
 
-#include "tmc/ex_cpu.hpp"
 namespace tmc {
 namespace test {
 /// The executor doesn't provide any condition variable for this, so just use a
@@ -16,35 +18,46 @@ namespace test {
 /// this should only be called after the benchmark has completed (by using
 /// post_waitable().wait() on the root task of the benchmark).
 size_t wait_for_all_threads_to_sleep(ex_cpu& CpuExecutor) {
-  size_t count = 0;
-  // maybe this doesn't work? WTBS can be spuriously zero?
-  // otherwise why does sleep_for behave very differently from mm_pause?
-  while (CpuExecutor.working_threads_bitset.load(std::memory_order_acquire) != 0
-  ) {
-    // this doesn't seem to work properly - increasing this to 10x results in
-    // 2x slowdown of the benchmark maybe because of core power state
-    // transition?
-    // or adding a single MM_PAUSE to the dequeue_ex_cpu loop also causes 2x
-    // slowdown
+  size_t waitCount = 0;
 
-    // on desktop - it works fine with either 1x, 10x, or 100x _mm_pause,
-    // or sleep_for(1ms) or sleep_for(10ms) - the benchmark timing is very
-    // consistent (no negative impact) in any scenarios, and it only needs to
-    // sleep/wait about 5/1000 runs
-
-    //     for (int i = 0; i < 100; ++i) {
-    // #if defined(__x86_64__) || defined(_M_AMD64)
-    //       _mm_pause();
-    // #endif
-    // #if defined(__arm__) || defined(_M_ARM) || defined(__aarch64__) || \
-//   defined(__ARM_ACLE)
-    //       __yield();
-    // #endif
-    //     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    ++count;
+  size_t runThreads = 0;
+  if (detail::this_thread::executor == CpuExecutor.type_erased()) {
+    // if this function is being called from an executor thread, we will never
+    // see ALL threads sleeping - at least this one will always be running
+    runThreads = 1;
   }
-  return count;
+
+  while (__builtin_popcountll(
+           CpuExecutor.working_threads_bitset.load(std::memory_order_acquire)
+         ) > runThreads) {
+
+    // on desktop 5950X or laptop 5900HX - this works fine with either 1x, 10x,
+    // or 100x _mm_pause, or sleep_for(1ms) or sleep_for(10ms) - the benchmark
+    // timing is very consistent (no negative impact) in any scenarios, and it
+    // only needs to sleep/wait about 5/1000 runs
+
+    // on server EPYC 7742, not the same - more pauses / longer sleep = slower
+    // benchmark (even though sleep time is not part of the benchmark time). I
+    // believe this is because the server CPUs go to sleep if you wait long
+    // enough, and have long transition latency to wake back up. The work is
+    // also more poorly distributed on server CPUs causing some threads to
+    // finish much sooner than others. Using `cpupower frequency-set -g
+    // performance` mitigates this somewhat. It also has to wait almost every
+    // loop iteration (995/1000 runs).
+
+    for (int i = 0; i < 4; ++i) {
+#if defined(__x86_64__) || defined(_M_AMD64)
+      _mm_pause();
+#endif
+#if defined(__arm__) || defined(_M_ARM) || defined(__aarch64__) ||             \
+  defined(__ARM_ACLE)
+      __yield();
+#endif
+    }
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    ++waitCount;
+  }
+  return waitCount;
 }
 } // namespace test
 } // namespace tmc
