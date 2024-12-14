0455c2987460cabf20749d0d99d1e55986eb672e
tzcnt <trailingzerocount@gmail.com>
2024-12-13 16:40:09 -0800

    update each() doc comment

--- include/tmc/spawn_many.hpp
+++ include/tmc/spawn_many.hpp
@@ -792,7 +792,14 @@ public:
   }
 
   /// Submits the wrapped tasks immediately, without suspending the current
-  /// coroutine. You must await the return type before destroying it.
+  /// coroutine. The results will be returned one at a time, as they become
+  /// ready. Each time this is co_awaited, it will return the index of a single
+  /// ready result. The result indexes correspond to the indexes of the
+  /// originally submitted tasks, and the values can be accessed using
+  /// `operator[]`. Results may become ready in any order, but when awaited
+  /// repeatedly, each index from `[0..task_count)` will be returned exactly
+  /// once. You must await this repeatedly until all tasks are complete, at
+  /// which point the index returned will be equal to the value of `end()`.
   inline aw_task_many_each<Result, Count> each() && {
 #ifndef NDEBUG
     assert(!did_await);
