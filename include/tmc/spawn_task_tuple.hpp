55265f169d89ab23ea332120df061ab342d425f3
tzcnt <trailingzerocount@gmail.com>
2024-12-13 17:48:18 -0800

    WIP tuple

--- include/tmc/spawn_task_tuple.hpp
+++ include/tmc/spawn_task_tuple.hpp
@@ -14,8 +14,14 @@
 #include <cassert>
 #include <coroutine>
 #include <tuple>
+#include <type_traits>
+#include <variant>
 
 namespace tmc {
+
+template <typename T>
+using void_to_monostate =
+  std::conditional_t<std::is_void_v<T>, std::monostate, T>;
 
 template <typename... Result> class aw_spawned_task_tuple_impl;
 
@@ -24,50 +30,10 @@ template <typename... Result> class aw_spawned_task_tuple_impl {
   detail::type_erased_executor* executor;
   detail::type_erased_executor* continuation_executor;
   size_t prio;
-  std::tuple<Result...> result;
+  std::tuple<void_to_monostate<Result>...> result;
   friend aw_spawned_task_tuple<Result...>;
   aw_spawned_task_tuple_impl(
-    task<Result>... Task, detail::type_erased_executor* Executor,
-    detail::type_erased_executor* ContinuationExecutor, size_t Prio
-  )
-      : wrapped{std::move(Task)...}, executor{Executor},
-        continuation_executor{ContinuationExecutor}, prio{Prio} {}
-
-public:
-  /// Always suspends.
-  inline bool await_ready() const noexcept { return false; }
-
-  /// Suspends the outer coroutine, submits the wrapped task to the
-  /// executor, and waits for it to complete.
-  TMC_FORCE_INLINE inline void await_suspend(std::coroutine_handle<> Outer
-  ) noexcept {
-#ifndef TMC_TRIVIAL_TASK
-    assert(wrapped);
-#endif
-    auto& p = wrapped.promise();
-    p.continuation = Outer.address();
-    p.continuation_executor = continuation_executor;
-    p.result_ptr = &result;
-    detail::post_checked(executor, std::move(wrapped), prio);
-  }
-
-  /// Returns the value provided by the wrapped task.
-  inline Result&& await_resume() noexcept {
-    // This appears to never be used - the 'this' parameter to
-    // await_resume() is always an lvalue
-    return std::move(result);
-  }
-};
-
-template <> class aw_spawned_task_tuple_impl<void> {
-  task<void> wrapped;
-  detail::type_erased_executor* executor;
-  detail::type_erased_executor* continuation_executor;
-  size_t prio;
-  friend aw_spawned_task_tuple<void>;
-
-  inline aw_spawned_task_tuple_impl(
-    task<void> Task, detail::type_erased_executor* Executor,
+    std::tuple<task<Result>...> Task, detail::type_erased_executor* Executor,
     detail::type_erased_executor* ContinuationExecutor, size_t Prio
   )
       : wrapped{std::move(Task)}, executor{Executor},
@@ -77,21 +43,33 @@ public:
   /// Always suspends.
   inline bool await_ready() const noexcept { return false; }
 
+  template <typename T>
+  TMC_FORCE_INLINE inline void
+  submit_task(tmc::task<T> Task, std::coroutine_handle<> Outer) {
+    auto& p = Task.promise();
+    p.continuation = Outer.address();
+    p.continuation_executor = continuation_executor;
+    p.result_ptr = &result;
+    detail::post_checked(executor, std::move(Task), prio);
+  }
+
   /// Suspends the outer coroutine, submits the wrapped task to the
   /// executor, and waits for it to complete.
   TMC_FORCE_INLINE inline void await_suspend(std::coroutine_handle<> Outer
   ) noexcept {
+    std::apply(
+      [this, Outer](auto&&... task) { ((submit_task(task, Outer)), ...); },
+      wrapped
+    );
 #ifndef TMC_TRIVIAL_TASK
     assert(wrapped);
 #endif
-    auto& p = wrapped.promise();
-    p.continuation = Outer.address();
-    p.continuation_executor = continuation_executor;
-    detail::post_checked(executor, std::move(wrapped), prio);
   }
 
-  /// Does nothing.
-  inline void await_resume() noexcept {}
+  /// Returns the value provided by the wrapped task.
+  inline std::tuple<void_to_monostate<Result>&&...> await_resume() noexcept {
+    return std::move(result);
+  }
 };
 
 // Primary template is forward-declared in "tmc/detail/aw_run_early.hpp".
@@ -100,13 +78,13 @@ class [[nodiscard(
   "You must use the aw_spawned_task_tuple<Result> by one of: 1. "
   "co_await 2. run_early()"
 )]] aw_spawned_task_tuple
-    : public detail::run_on_mixin<aw_spawned_task_tuple<Result>>,
-      public detail::resume_on_mixin<aw_spawned_task_tuple<Result>>,
-      public detail::with_priority_mixin<aw_spawned_task_tuple<Result>> {
-  friend class detail::run_on_mixin<aw_spawned_task_tuple<Result>>;
-  friend class detail::resume_on_mixin<aw_spawned_task_tuple<Result>>;
-  friend class detail::with_priority_mixin<aw_spawned_task_tuple<Result>>;
-  task<Result> wrapped;
+    : public detail::run_on_mixin<aw_spawned_task_tuple<Result...>>,
+      public detail::resume_on_mixin<aw_spawned_task_tuple<Result...>>,
+      public detail::with_priority_mixin<aw_spawned_task_tuple<Result...>> {
+  friend class detail::run_on_mixin<aw_spawned_task_tuple<Result...>>;
+  friend class detail::resume_on_mixin<aw_spawned_task_tuple<Result...>>;
+  friend class detail::with_priority_mixin<aw_spawned_task_tuple<Result...>>;
+  task<Result...> wrapped;
   detail::type_erased_executor* executor;
   detail::type_erased_executor* continuation_executor;
   size_t prio;
@@ -114,13 +92,13 @@ class [[nodiscard(
 public:
   /// It is recommended to call `spawn()` instead of using this constructor
   /// directly.
-  aw_spawned_task_tuple(task<Result>&& Task)
-      : wrapped(std::move(Task)), executor(detail::this_thread::executor),
+  aw_spawned_task_tuple(task<Result>&&... Task)
+      : wrapped(std::move(Task)...), executor(detail::this_thread::executor),
         continuation_executor(detail::this_thread::executor),
         prio(detail::this_thread::this_task.prio) {}
 
-  aw_spawned_task_tuple_impl<Result> operator co_await() && {
-    return aw_spawned_task_tuple_impl<Result>(
+  aw_spawned_task_tuple_impl<Result...> operator co_await() && {
+    return aw_spawned_task_tuple_impl<Result...>(
       std::move(wrapped), executor, continuation_executor, prio
     );
   }
