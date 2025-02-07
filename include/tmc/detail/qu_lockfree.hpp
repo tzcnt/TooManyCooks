0275a1324268385f9e15a28ec9a2bfcdcf16be45
tzcnt <trailingzerocount@gmail.com>
2025-02-06 23:57:53 -0800

    fix bug in qu_lockfree when used outside ex_cpu

--- include/tmc/detail/qu_lockfree.hpp
+++ include/tmc/detail/qu_lockfree.hpp
@@ -1606,17 +1606,17 @@ public:
   bool empty() const {
     // TODO make a producer thread version of this that uses this thread's
     // static iteration order
-    auto static_producer_count = dequeueProducerCount - 1;
-    ExplicitProducer* producers = staticProducers;
-    if (dequeueProducerCount == 0) {
-      return true;
-    }
-    for (size_t pidx = 0; pidx < static_producer_count; ++pidx) {
-      ExplicitProducer& prod = producers[pidx];
-      if (prod.size() != 0) {
-        return false;
+    if (dequeueProducerCount != 0) {
+      auto static_producer_count = dequeueProducerCount - 1;
+      ExplicitProducer* producers = staticProducers;
+      for (size_t pidx = 0; pidx < static_producer_count; ++pidx) {
+        ExplicitProducer& prod = producers[pidx];
+        if (prod.size() != 0) {
+          return false;
+        }
       }
     }
+
     for (auto ptr = producerListTail.load(std::memory_order_seq_cst);
          ptr != nullptr; ptr = ptr->next_prod()) {
       if (ptr->size() != 0) {
