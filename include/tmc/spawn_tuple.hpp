a4f69812fcecb4673c4b9fda4bc87431bea99a91
tzcnt <trailingzerocount@gmail.com>
2025-10-01 20:35:53 -0700

    add attribute to spawn_tuple

--- include/tmc/spawn_tuple.hpp
+++ include/tmc/spawn_tuple.hpp
@@ -279,8 +279,8 @@ public:
 
   /// Suspends the outer coroutine, submits the wrapped task to the
   /// executor, and waits for it to complete.
-  inline std::coroutine_handle<> await_suspend(std::coroutine_handle<> Outer
-  ) noexcept
+  inline std::coroutine_handle<>
+  await_suspend(std::coroutine_handle<> Outer) noexcept
     requires(!IsEach)
   {
 #ifndef NDEBUG
@@ -401,7 +401,8 @@ using aw_spawn_tuple_each =
   tmc::detail::lvalue_only_awaitable<aw_spawn_tuple_impl<true, Result...>>;
 
 template <typename... Awaitable>
-class [[nodiscard("You must await or initiate the result of spawn_tuple()."
+class [[nodiscard(
+  "You must await or initiate the result of spawn_tuple()."
 )]] aw_spawn_tuple
     : public tmc::detail::run_on_mixin<aw_spawn_tuple<Awaitable...>>,
       public tmc::detail::resume_on_mixin<aw_spawn_tuple<Awaitable...>>,
@@ -583,7 +584,7 @@ public:
 /// Does not support non-awaitable types (such as regular functors).
 template <typename... Awaitable>
 aw_spawn_tuple<tmc::detail::forward_awaitable<Awaitable>...>
-spawn_tuple(Awaitable&&... Tasks) {
+spawn_tuple([[clang::coro_await_elidable_argument]] Awaitable&&... Tasks) {
   return aw_spawn_tuple<tmc::detail::forward_awaitable<Awaitable>...>(
     std::forward_as_tuple(static_cast<Awaitable&&>(Tasks)...)
   );
