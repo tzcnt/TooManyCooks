1633f915faa636bbd1ea4be547300dd19db5aed4
tzcnt <trailingzerocount@gmail.com>
2026-02-01 15:27:39 -0800

    update doc comments

--- include/tmc/traits.hpp
+++ include/tmc/traits.hpp
@@ -27,10 +27,10 @@ using unknown_t = tmc::detail::unknown_t;
 /// This detection is based on the presence of `await_resume()`
 /// or `operator co_await()` methods.
 ///
-/// Note: For an unknown (non-TMC) awaitable, when passed template type T, this
-/// will detect unqualified or rvalue-qualified member functions. If your
-/// awaitable type only has lvalue-qualified `await_resume() &`
-///  or `operator co_await() &`, you must pass template type T&.
+/// Note: For an unknown (non-TMC) awaitable with `operator co_await()`, when
+/// passed template type T, this will detect unqualified or rvalue-qualified
+/// member functions. If your awaitable type only has lvalue-qualified
+/// `operator co_await() &`, you must pass template type T&.
 /// See the `is_awaitable_unknown_*` tests for example:
 /// https://github.com/tzcnt/tmc-examples/blob/main/tests/test_traits.cpp#L87
 ///
@@ -45,14 +45,15 @@ using awaitable_result_t = tmc::detail::awaitable_result_t<Awaitable>;
 
 /*** Callable Traits ***/
 
-/// Constrains types to those that are callable and NOT awaitable.
-/// Types must expose `operator()()` with no parameters, and not expose any
-/// `await_*()` or `operator co_await()` functions.
+/// Constrains types to those that are invocable with no arguments and NOT
+/// awaitable (according to `tmc::is_awaitable`).
+/// This is derived using `std::invoke_result_t<T&>`, which includes function
+/// pointers, lambdas, `std::function`, and any type with `operator()()`.
 template <typename T>
 concept is_callable = tmc::detail::is_callable<T>;
 
-/// Get the result type of calling `T::operator()()`.
-/// This is derived using `std::invoke_result_t<T&>`.
+/// Get the result type of invoking `Callable` with no arguments.
+/// This is derived using `std::invoke_result_t<Callable&>`.
 template <typename Callable>
 using callable_result_t = tmc::detail::func_result_t<Callable>;
 
@@ -60,9 +61,10 @@ using callable_result_t = tmc::detail::func_result_t<Callable>;
 
 using executable_kind = tmc::detail::executable_kind;
 
-/// - If T is an awaitable type, returns AWAITABLE.
-/// - Else if T is a callable type, returns CALLABLE.
-/// - Else returns UNKNOWN.
+/// Returns the `executable_kind` enum value for type T:
+/// - If T is an awaitable type, returns `executable_kind::AWAITABLE`.
+/// - Else if T is a callable type, returns `executable_kind::CALLABLE`.
+/// - Else returns `executable_kind::UNKNOWN`.
 template <typename T>
 static inline constexpr executable_kind executable_kind_v =
   tmc::detail::executable_kind_v<T>;
@@ -104,24 +106,26 @@ concept is_task_nonvoid = tmc::detail::is_task_nonvoid<T>;
 template <typename T, typename Result>
 concept is_task_result = tmc::detail::is_task_result<T, Result>;
 
-/*** Functor Traits ***/
+/*** Invocable Traits ***/
 
-/// Constrains types to functors with `operator()()` that aren't a `tmc::task`.
+/// Constrains types to invocables with no arguments that aren't a `tmc::task`.
+/// Note: a coroutine function that returns a `tmc::task` would count
+/// as `is_func == true`, but the `tmc::task` object itself (which
+/// has `void operator()()`) will return `is_func == false`.
 template <typename T>
 concept is_func = tmc::detail::is_func<T>;
 
-/// Constrains types to functors with `void operator()()` that aren't a
-/// `tmc::task`.
+/// Constrains types to invocables with no arguments that return `void`, and
+/// aren't a `tmc::task`.
 template <typename T>
 concept is_func_void = tmc::detail::is_func_void<T>;
 
-/// Constrains types to functors with `Result operator()()` (Result != void)
-/// that aren't a `tmc::task`.
+/// Constrains types to invocables with no arguments that return a non-void
+/// result type.
 template <typename T>
 concept is_func_nonvoid = tmc::detail::is_func_nonvoid<T>;
 
-/// Constrains types to functors with `Result operator()()` that aren't a
-/// `tmc::task`.
+/// Constrains types to invocables with no arguments returning `Result`.
 template <typename T, typename Result>
 concept is_func_result = tmc::detail::is_func_result<T, Result>;
 
